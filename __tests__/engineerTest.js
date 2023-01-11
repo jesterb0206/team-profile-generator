9923f618b9eb3714b1454f95117da3222bec3723
jesterb0206 <jesterb@seattleu.edu>
2023-01-11 15:53:02 -0800

    Created The Engineer Jest Test

---
+++ __tests__/engineerTest.js
@@ -0,0 +1,44 @@
+// Import Engineer Constructor
+
+const Engineer = require('../lib/engineer');
+
+// Creates An Engineer Object
+
+test('Creates an Engineer Object', () => {
+  const engineer = new Engineer(
+    'Bradley',
+    7,
+    'jesterb@seattleu.edu',
+    'jesterb0206'
+  );
+
+  expect(engineer.github).toEqual(expect.any(String));
+});
+
+// Gets GitHub From getGithub()
+
+test('Gets Engineer GitHub Account', () => {
+  const engineer = new Engineer(
+    'Bradley',
+    7,
+    'jesterb@seattleu.edu',
+    'jesterb0206'
+  );
+
+  expect(engineer.getGithub()).toEqual(
+    expect.stringContaining(engineer.github.toString())
+  );
+});
+
+// Gets Role From getRole ()
+
+test('Gets Role of Employee', () => {
+  const engineer = new Engineer(
+    'Bradley',
+    7,
+    'jesterb@seattleu.edu',
+    'jesterb0206'
+  );
+
+  expect(engineer.getRole()).toEqual('Engineer');
+});
