3cb281cdf1df8b98860f9de6414bbf76df4d040a
jesterb0206 <jesterb@seattleu.edu>
2023-01-11 16:05:03 -0800

    Created The Intern Jest Test

---
+++ __tests__/internTest.js
@@ -0,0 +1,29 @@
+// Import Intern Constructor
+
+const Intern = require('../lib/intern');
+
+// Creates An Intern Object
+
+test('Creates an Intern Object', () => {
+  const intern = new Intern('Bradley', 7, 'jesterb@seattleu.edu', 'UW');
+
+  expect(intern.school).toEqual(expect.any(String));
+});
+
+// Gets School From getSchool()
+
+test('Gets Intern School', () => {
+  const intern = new Intern('Bradley', 7, 'jesterb@seattleu.edu', 'UW');
+
+  expect(intern.getSchool()).toEqual(
+    expect.stringContaining(intern.school.toString())
+  );
+});
+
+// Gets Role From getRole()
+
+test('Gets Role of Employee', () => {
+  const intern = new Intern('Bradley', 7, 'jesterb@seattleu.edu', 'UW');
+
+  expect(intern.getRole()).toEqual('Intern');
+});
